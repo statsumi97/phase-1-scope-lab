06cf00b581e6d4565188ee3d98eacc812426b296
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-11-03 20:43:34 -0400

    final product

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+//
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+//
+var bestCustomer = undefined;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+//
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+//
+const leastFavoriteCustomer = 'steve';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bob';
+}
+
+/*
+describe('changeLeastFavoriteCustomer()', function() {
+    it('unsuccessfully tries to reassign the least favorite customer', function() {
+      expect(changeLeastFavoriteCustomer).to.throw('Assignment to constant variable.');
+    });
+  });
+});
+  */
